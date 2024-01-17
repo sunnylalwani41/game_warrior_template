171dadd8dc16ad53c60f3549c6f0b27e31523958
digitaladdworlds <digitaladdworld799@gmail.com>
2024-01-17 10:43:32 +0530

    Update in Day1

---
+++ src/main/java/com/gamewarrior/Game/Warrior/model/DepositRequest.java
@@ -0,0 +1,24 @@
+package com.gamewarrior.Game.Warrior.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Data;
+
+@Data
+@Entity
+public class DepositRequest {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "requestGenerator")
+	@SequenceGenerator(name = "requestGenerator", sequenceName ="requestGen", allocationSize = 1, initialValue = 100000)
+	private Integer id;
+	private Integer userId;
+	private String upiId;
+	private String upiName;
+	private String path;
+	private Integer amount;
+	private Boolean status=false;
+	private String remark;
+}
